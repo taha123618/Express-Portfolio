33267be4035ad542a004b5166d68548ec0fbba23
taha <tahaahmedanees2@gmail.com>
2021-08-22 16:46:41 +0500

    portfolio website

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const hbs = require("hbs"); 
+const path = require("path");
+const port = process.env.PORT || 8084;
+
+
+// use satic content 
+const satic_Path = path.join(__dirname,"../public");
+// partial 
+const tempview_Path = path.join(__dirname,"../temp/views");
+// use parial template engine 
+const partials_path = path.join(__dirname,"../temp/partial");
+
+
+app.use(express.static(satic_Path));
+app.set('view engine', 'hbs');
+app.set('views',tempview_Path);
+hbs.registerPartials(partials_path);
+
+
+
+
+// routhing
+app.get("/",(req,res) => {
+    res.render("index.hbs");
+     });
+
+
+         app.get("/work",(req,res) => {
+          res.render("portfolio.hbs");
+           });
+    
+    //  404
+    app.get("*",(req,res) => {
+        res.render("404.hbs",{
+         errmsg : "Opps! You Are Visit Wrong page"
+        });
+      //   res.send("Opps! PAge Not Found");
+        }); 
+
+        // listinnng 
+
+ app.listen(port,() => {
+    console.log(`listing.... to ${port}`)
+ });
